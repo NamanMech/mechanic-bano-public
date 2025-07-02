f14d09557e36590a5c35e930b10c4ad6263dd675
NamanMech <mechanicseekho@gmail.com>
2025-07-02 14:08:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Navbar from './components/Navbar'; // Import kiya
 
 export default function App() {
   return (
     <Router>
-      <header>
-        <h1>Mechanic Bano - Bike Repair Tutorials</h1>
-      </header>
+      <Navbar /> {/* Yaha navbar lagaya */}
       <div className="container">
         <Routes>
           <Route path="/" element={<Home />} />
+          {/* 404 Page yaha future me add hoga */}
         </Routes>
       </div>
     </Router>
