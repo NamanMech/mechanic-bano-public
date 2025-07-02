73756c7f19e2d4b66440677e2856abd6906697eb
NamanMech <mechanicseekho@gmail.com>
2025-07-02 12:55:03 +0530

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import VideoList from './VideoList';
+
+export default function Home() {
+  return (
+    <div>
+      <h2>All Tutorials</h2>
+      <VideoList />
+    </div>
+  );
+}
