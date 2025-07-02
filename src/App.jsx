dd46696d125dfcf17e6cdcb6b928b688d65009c3
NamanMech <mechanicseekho@gmail.com>
2025-07-02 17:14:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
-import Navbar from './components/Navbar'; // Import kiya
+import PDFs from './pages/PDFs';
 
 export default function App() {
   return (
     <Router>
-      <Navbar /> {/* Yaha navbar lagaya */}
+      <header>
+        <h1>Mechanic Bano - Bike Repair Tutorials</h1>
+        <nav style={{ marginTop: '10px' }}>
+          <Link to="/" style={{ color: 'white', marginRight: '15px' }}>Home</Link>
+          <Link to="/pdfs" style={{ color: 'white' }}>PDFs</Link>
+        </nav>
+      </header>
       <div className="container">
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* 404 Page yaha future me add hoga */}
+          <Route path="/pdfs" element={<PDFs />} />
         </Routes>
       </div>
     </Router>
