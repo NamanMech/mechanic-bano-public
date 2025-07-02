b69923463fa556cade168a1d70afe23862211d85
NamanMech <mechanicseekho@gmail.com>
2025-07-03 01:16:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,43 @@
-// src/App.jsx
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
-import Videos from './pages/VideoList';
-import Pdf from './pages/pdf';
+import VideoList from './pages/VideoList';
+import PDFList from './pages/pdf';
+import axios from 'axios';
 
 export default function App() {
+  const [siteName, setSiteName] = useState('Mechanic Bano');
+
+  const fetchSiteName = async () => {
+    try {
+      const response = await axios.get('https://mechanic-bano-backend.vercel.app/api/sitename');
+      if (response.data.length > 0) {
+        setSiteName(response.data[0].name);
+      }
+    } catch (error) {
+      console.error('Error fetching site name');
+    }
+  };
+
+  useEffect(() => {
+    fetchSiteName();
+  }, []);
+
   return (
     <Router>
       <header>
-        <h1>Mechanic Bano</h1>
+        <h1>{siteName}</h1>
         <nav style={{ marginTop: '10px' }}>
-          <Link to="/" style={{ marginRight: '15px' }}>Home</Link>
-          <Link to="/videos" style={{ marginRight: '15px' }}>Videos</Link>
-          <Link to="/pdf" style={{ marginRight: '15px' }}>PDFs</Link>
+          <Link to="/" style={{ marginRight: '15px', color: 'white' }}>Home</Link>
+          <Link to="/videos" style={{ marginRight: '15px', color: 'white' }}>Videos</Link>
+          <Link to="/pdfs" style={{ color: 'white' }}>PDFs</Link>
         </nav>
       </header>
       <div className="container">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/videos" element={<Videos />} />
-          <Route path="/pdf" element={<Pdf />} />
+          <Route path="/videos" element={<VideoList />} />
+          <Route path="/pdfs" element={<PDFList />} />
         </Routes>
       </div>
     </Router>
