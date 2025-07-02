feaffb98f6baf09725389e64a8bf90cdacf4f7cb
NamanMech <mechanicseekho@gmail.com>
2025-07-03 00:49:10 +0530

    Update Spinner.jsx

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -2,5 +2,10 @@
 
 import React from 'react';
 
-export default function Spinner() { return ( <div style={{ textAlign: 'center', marginTop: '50px' }}> <div className="loader"></div> </div> ); }
-
+export default function Spinner() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}>
+      <div className="loader"></div>
+    </div>
+  );
+}
