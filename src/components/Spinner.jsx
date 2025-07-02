a68dc5fa9fa24cfb8ebe7a2d7b103668df40734f
NamanMech <mechanicseekho@gmail.com>
2025-07-03 00:45:14 +0530

    Create Spinner.jsx

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,6 @@
+// src/components/Spinner.jsx 
+
+import React from 'react';
+
+export default function Spinner() { return ( <div style={{ textAlign: 'center', marginTop: '50px' }}> <div className="loader"></div> </div> ); }
+
