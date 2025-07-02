6f57fffe1ec40d49a4ff53a285ee829140e754a1
NamanMech <mechanicseekho@gmail.com>
2025-07-02 22:58:39 +0530

    Create pdf.jsx

---
+++ src/pages/pdf.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PDFList from '../components/PDFList';
+
+export default function Pdf() {
+  return (
+    <div>
+      <h2>Bike Repair PDFs</h2>
+      <PDFList />
+    </div>
+  );
+}
