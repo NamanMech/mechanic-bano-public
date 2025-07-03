4d90c54ab4e565fede6653f7f7bf2d42707aa5ad
NamanMech <mechanicseekho@gmail.com>
2025-07-04 04:04:41 +0530

    Update GoogleAuth.jsx

--- src/components/GoogleAuth.jsx
+++ src/components/GoogleAuth.jsx
@@ -1,22 +1,21 @@
 // src/components/GoogleAuth.jsx
 import React from 'react';
-import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
+import { GoogleLogin } from '@react-oauth/google';
 import jwt_decode from 'jwt-decode';
 
-export default function GoogleAuth({ onSuccess }) {
+export default function GoogleAuth({ onLogin }) {
   return (
-    <GoogleOAuthProvider clientId="303726026304-0q7mt0gsqervgmkgp8tkck7nviluek1l.apps.googleusercontent.com">
-      <div style={{ display: 'flex', justifyContent: 'center', margin: '20px 0' }}>
-        <GoogleLogin
-          onSuccess={credentialResponse => {
-            const decoded = jwt_decode(credentialResponse.credential);
-            onSuccess(decoded);
-          }}
-          onError={() => {
-            alert('Login Failed');
-          }}
-        />
-      </div>
-    </GoogleOAuthProvider>
+    <div className="login-container">
+      <GoogleLogin
+        onSuccess={credentialResponse => {
+          const decoded = jwt_decode(credentialResponse.credential);
+          localStorage.setItem('user', JSON.stringify(decoded));
+          onLogin(decoded);
+        }}
+        onError={() => {
+          alert('Login Failed');
+        }}
+      />
+    </div>
   );
 }
